2f01c83fb19864213fa15afe5836a1ff2868288e
Weixing-Zhang <weixing.zhang365@gmail.com>
2025-06-22 21:35:20 -0400

    Reverted the changes to unrelated module

--- rust/mlt/src/decoder/varint.rs
+++ rust/mlt/src/decoder/varint.rs
@@ -1,6 +1,5 @@
-use bytes_varint::*;
-
 use crate::decoder::tracked_bytes::TrackedBytes;
+use bytes_varint::*;
 
 pub fn decode(input: &mut TrackedBytes, num_values: usize) -> Vec<u32> {
     let mut values = Vec::with_capacity(num_values);
